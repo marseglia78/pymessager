cdea203267ab3c44a9b4858c2a362e367faedf7d
Marseglia <lumarseglia@deloitte.com>
2018-09-25 18:05:16 -0700

    commit

--- RUN_GIT.bat
+++ RUN_GIT.bat
@@ -2,6 +2,6 @@ git init
 git add .
 git commit -m "commit"
 #git remote add origin https://github.com/marseglia78/pymessager.git
-#git remote -v
+git remote -v
 git push origin master
 heroku logs --tail -a luca-pymessager
